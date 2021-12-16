e76dc0fbed8127d6753ebe478e3dd996ee8bd1c1
Stefanlet100k <88986988+Stefanlet100k@users.noreply.github.com>
2021-12-16 20:26:48 +0100

    Update FundRise.sol

--- FundRise.sol
+++ FundRise.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.7.0 <0.9.0;
+pragma solidity >=0.6.6 <0.9.0;
 
+import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.6/vendor/SafeMathChainlink.sol";
 
 contract fundMe{
+
+    using SafeMathChainlink for uint256;
+
     address public owner;
     struct  parameters{
         string description;
@@ -16,6 +21,12 @@ contract fundMe{
     mapping(uint => parameters) Founds;
 
     uint public noOfFound;
+
+    function getEthPrice() public view returns(int256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        (,int256 answer,,,) =  priceFeed.latestRoundData(); 
+        return answer/10**8;
+    }
     
     function setFound(string memory _goal, uint _goalAmount, uint _minVal, uint _deadline) public{
         parameters storage newFound = Founds[noOfFound];
@@ -38,14 +49,17 @@ contract fundMe{
     function finalizeFund(uint _no) public {
         require(msg.sender == Founds[_no].owner);
         payable(Founds[_no].owner).transfer(Founds[_no].amount);
+        Founds[_no].amount = 0;
     }
 
 
-    function getBalance(uint _no) public view returns(uint){
-        return Founds[_no].amount;
+    function getParams(uint _no) public view returns(string memory, uint, uint){
+        return (Founds[_no].description, Founds[_no].amount, Founds[_no].deadline);
     }
 
-    function getDescription(uint _no) public view returns(string memory){
-        return Founds[_no].description;
+
+    function getVersion() public view returns(uint256){
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x8A753747A1Fa494EC906cE90E9f37563A8AF630e);
+        return priceFeed.version();
     }
 }
